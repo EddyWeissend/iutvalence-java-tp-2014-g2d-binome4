f6364c736db6af54015da279776bfdb085a2eb22
EddyWeissend <eddy.weissend@iut-valence.fr>
2014-02-17 10:49:43 +0100

    Apport a la classe LanceurDeMemory

--- src/JeuMemory/LanceurDeMemory.java
+++ src/JeuMemory/LanceurDeMemory.java
@@ -8,6 +8,12 @@ public class LanceurDeMemory
 {
 	public static void main(String[] args)
 	{
+		System.out.println("Creation de la partie");
 		Memory partie = new Memory();
+		
+		System.out.println("Demarrage de la partie");
+		partie.jouer();
+		
+		System.out.println("Fin de la partie");
 	}
 }
