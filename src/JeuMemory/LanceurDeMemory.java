764a00f2eb30af1fdef73ab3aadb52479f35b5ab
EddyWeissend <eddy.weissend@iut-valence.fr>
2014-02-17 10:35:04 +0100

    Déplacement de LanceurDeMemory
    Déplacement du paquetage par défaut vers le paquetage JeuMemory

--- src/JeuMemory/LanceurDeMemory.java
+++ src/JeuMemory/LanceurDeMemory.java
@@ -1,9 +1,13 @@
+package JeuMemory;
 /**
  * Permet de jouer une partie de memory
  * @author weissene
  *
  */
-public class Memory
+public class LanceurDeMemory
 {
-
+	public static void main(String[] args)
+	{
+		Memory partie = new Memory();
+	}
 }
