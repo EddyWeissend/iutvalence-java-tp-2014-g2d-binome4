058e678883805af362a788c8908fc5dca0a5d9c6
EddyWeissend <eddy.weissend@iut-valence.fr>
2014-02-24 11:03:10 +0100

    Modification du constructeur Plateau()

--- src/fr/iutvalence/java/tp/Memory/Plateau.java
+++ src/fr/iutvalence/java/tp/Memory/Plateau.java
@@ -18,35 +18,36 @@ public class Plateau
 	 * Largeur du plateau
 	 */
 	private final static int LARGEUR = 4;
-	
+
 	/**
 	 * Nombre de cases du plateau
 	 */
 
-	private final static int NOMBRE_DE_CASES_DU_PLATEAU = LONGUEUR*LARGEUR;
-	
+	private final static int NOMBRE_DE_CASES_DU_PLATEAU = LONGUEUR * LARGEUR;
+
 	/**
 	 * Représente le nombre de cartes présentes sur le plateau
 	 */
 	private int nombreDeCartesPresenteSurLePlateau;
-	
+
 	/**
 	 * Tableau où sont stockés les cartes du jeu
 	 */
 	private Carte[][] lesCartesDuJeu;
 
 	/**
-	 * Création d'un plateau de jeu :
-	 * - un certain de cartes retournées
-	 * -
+	 * Création d'un plateau de jeu : - un certain de cartes retournées -
 	 */
 	public Plateau()
 	{
-		this.nombreDeCartesPresenteSurLePlateau=NOMBRE_DE_CASES_DU_PLATEAU;
+		this.nombreDeCartesPresenteSurLePlateau = NOMBRE_DE_CASES_DU_PLATEAU;
 		this.lesCartesDuJeu = new Carte[LONGUEUR][LARGEUR];
-				
+		for (int indiceColonne = 0; indiceColonne < LONGUEUR; indiceColonne++)
+			for (int indiceLigne = 0; indiceLigne < LARGEUR; indiceLigne++)
+				this.lesCartesDuJeu[indiceColonne][indiceLigne] = new Carte();
+
 	}
-	
+
 	/**
 	 * @return le nombre de cartes présentes sur le plateau
 	 */
