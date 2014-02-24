e4654bec172a344159f6f036492c67d2520f857b
EddyWeissend <eddy.weissend@iut-valence.fr>
2014-02-24 11:19:47 +0100

    Complétion de la classe Carte

--- src/fr/iutvalence/java/tp/Memory/Carte.java
+++ src/fr/iutvalence/java/tp/Memory/Carte.java
@@ -1,11 +1,43 @@
 package fr.iutvalence.java.tp.Memory;
 
 /**
+ * Représentation d'une carte de jeu Memory
  * 
  * @author weissene
- *
+ * 
  */
 public class Carte
 {
+	/**
+	 * Face de la carte par défaut
+	 */
+	private static final boolean FACE_PAR_DEFAUT = false;
+	/**
+	 * Intermédiaire pour créer un symbole aléatoire
+	 */
+	public final int VALEUR_MINIMALE = 1;
+	/**
+	 * Intermédiaire pour créer un symbole aléatoire
+	 */
+	public final int VALEUR_MAXIMALE = 13;
+	/**
+	 * Indique si la carte est retournée ou non :
+	 * -false si la carte est face cachée
+	 * -true si la carte est face visible 
+	 */
+	private boolean estRetournee;
+	/**
+	 * Symbole de la carte compris entre 1 et 12 
+	 * car il y a 24 cartes donc 12 paires
+	 */
+	private int symbole;
 
+	/**
+	 * Créer une carte face cachée avec un symbole aléatoire
+	 */
+	public Carte()
+	{
+		this.estRetournee = FACE_PAR_DEFAUT;
+		this.symbole = (int)(Math.random() * (VALEUR_MAXIMALE-VALEUR_MINIMALE)) + VALEUR_MINIMALE;
+	}
 }
