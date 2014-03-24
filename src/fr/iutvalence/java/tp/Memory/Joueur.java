e2b4666bcac639ce1971a5ecebf7c81d5fe00f70
EddyWeissend <eddy.weissend@iut-valence.fr>
2014-03-24 11:21:48 +0100

    Création de la classe Joueur

---
+++ src/fr/iutvalence/java/tp/Memory/Joueur.java
@@ -0,0 +1,17 @@
+package fr.iutvalence.java.tp.Memory;
+
+public class Joueur
+{
+	private String nomDuJoueur;
+	
+	private int nombreDePairesTrouvees;
+
+	public Joueur(String nomDuJoueur)
+	{
+		this.nomDuJoueur=nomDuJoueur;
+		this.nombreDePairesTrouvees=0;
+	}
+	
+
+
+}
